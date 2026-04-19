0c830c8a7e49c95c1b021120a665eed6cc13d39a
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-04-18 17:08:27 -0700

    Travel Journal - header and entry component complete

---
+++ travel-journal/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="page-header">
+      <img src="img/globe.png" alt="Earth-logo" className="header-logo"/>
+      <h1 className="title">my travel journal.</h1>
+    </header>
+  )
+}
